67b19ed2b5029d0234617e597aad0364c646295e
tugui <350247720@qq.com>
2020-06-23 10:46:14 +0800

    update

--- train_imagenet.py
+++ train_imagenet.py
@@ -109,7 +109,7 @@ def main():
 
     # optionally resume from a checkpoint
     if args.resume:
-        if os.path.isfile(args.resume):
+        if os.path.exists(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
             args.start_epoch = checkpoint['epoch']
